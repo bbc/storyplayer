aa758c5c9c66b1ac0e6c3a0d0d81ebcdc985b98a
Angie Merryweather <angie.merryweather@bbc.co.uk>
2017-09-22 17:20:22 +0100

    Use shared ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        node: true,
+        browser: true,
+        mocha: true,
+        jasmine: true,
+        es6: true,
+    },
+    parser: 'babel-eslint',
+    plugins: ['babel', 'flowtype'],
+    extends: ['airbnb-base', 'plugin:flowtype/recommended'],
+
+    // override Airbnb defaults with our own rules
+    rules: {
+        indent: [2, 4],
+        'max-len': [1, 100], // downgrade this to a warning
+        'no-confusing-arrow': [1, { allowParens: true }],
+        'no-prototype-builtins': [0],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
+};
