e04103b95aa671aa30e0a640603d7dacedf09d74
Thomas Preece <thomaspreeceBBC@users.noreply.github.com>
2018-02-12 13:47:41 +0000

    Memory leak cleanup (#38)
    Cleanup of SimpleAV to remove everything correctly. Convert SimpleAV and BackgroundAudio to use pool of Hls instances. This has added advantage of also abstracting Hls.js so it can be replaced with dash.js if needed at a later date.
    
    This code fixes a good proportion of leakage on Linux Chromium and almost all leakage on Linux Firefox. When merged I will test on Windows Chrome and Firefox. The remaining leakage in Linux Chromium still happens when Hls.js is replaced with dash.js so I suspect it's a Linux Chrome specific issue (MSE buffer not being cleaned by browser).

---
+++ src/HlsManager.js
@@ -0,0 +1,125 @@
+// @flow
+
+// @flowignore
+import uuidv4 from 'uuid/v4';
+import Hls from 'hls.js';
+import logger from './logger';
+
+// Class wraps Hls giving it a unique id
+export class HlsInstance {
+    _hls: Object
+    _id: string
+    _eventList: Array<Object>
+    _attached: boolean
+
+    constructor(config: Object) {
+        this._hls = new Hls(config);
+        this._id = uuidv4();
+        this._eventList = [];
+    }
+
+    getId(): string {
+        return this._id;
+    }
+
+    // Copy existing Hls methods
+    loadSource(src: string) {
+        this._hls.loadSource(src);
+    }
+
+    attachMedia(videoElement: HTMLVideoElement) {
+        this._hls.attachMedia(videoElement);
+    }
+
+    detachMedia() {
+        this._hls.detachMedia();
+    }
+
+    on(event: string, callback: Function) {
+        this._hls.on(event, callback);
+        this._eventList.push({
+            event,
+            callback,
+        });
+    }
+
+    flush() {
+        // Manual force flush of buffer.
+        const bufferController = this._hls.coreComponents[4];
+        bufferController.doFlush();
+    }
+
+    clearEvents() {
+        // Cleanup all events added to hls
+        this._eventList.forEach((eventListObject) => {
+            this._hls.off(eventListObject.event, eventListObject.callback);
+        });
+    }
+}
+
+export default class HlsManager {
+    _hlsPool: Array<Object>
+    _defaultConfig: Object
+
+    constructor() {
+        this._hlsPool = [];
+        this._defaultConfig = {
+            startFragPrefetch: true,
+            startLevel: 3,
+            debug: false,
+        };
+    }
+
+    static get Events() {
+        return {
+            MANIFEST_PARSED: Hls.Events.MANIFEST_PARSED,
+        };
+    }
+
+    getHlsFromPool(): HlsInstance {
+        let useExistingPoolIndex = -1;
+        this._hlsPool.some((HlsPoolObject, index: number) => {
+            if (HlsPoolObject.active === false) {
+                useExistingPoolIndex = index;
+                return true;
+            }
+            return false;
+        });
+
+        // Use existing pool instance
+        if (useExistingPoolIndex !== -1) {
+            logger.info(`HLS Taken from Pool. Total HLS Instances: ${this._hlsPool.length}`);
+            this._hlsPool[useExistingPoolIndex].active = true;
+            return this._hlsPool[useExistingPoolIndex].hlsInstance;
+        }
+        // Create new pool instance
+        const newHls = new HlsInstance(this._defaultConfig);
+        this._hlsPool.push({
+            hlsInstance: newHls,
+            active: true,
+        });
+        logger.info(`New HLS Created. Total HLS Instances: ${this._hlsPool.length}`);
+        return newHls;
+    }
+
+
+    returnHlsToPool(hls: HlsInstance) {
+        hls.clearEvents();
+        hls.flush();
+
+        hls.detachMedia();
+
+        const id = hls.getId();
+        this._hlsPool.forEach((HlsPoolObject, index: number) => {
+            if (HlsPoolObject.hlsInstance.getId() === id) {
+                this._hlsPool[index].active = false;
+                logger.info(`HLS Returned to Pool. Total HLS Instances: ${this._hlsPool.length}`);
+            }
+        });
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    isSupported() {
+        return Hls.isSupported();
+    }
+}
--- src/renderers/SimpleAVRenderer.js
+++ src/renderers/SimpleAVRenderer.js
@@ -5,8 +5,7 @@ import BaseRenderer from './BaseRenderer';
 import type { Representation, AssetCollectionFetcher, MediaFetcher } from '../romper';
 import type { AnalyticsLogger } from '../AnalyticEvents';
 
-// @flowignore
-import Hls from '../../node_modules/hls.js/dist/hls';
+import HlsManager from '../HlsManager';
 import logger from '../logger';
 
 export default class SimpleAVRenderer extends BaseRenderer {
@@ -20,6 +19,8 @@ export default class SimpleAVRenderer extends BaseRenderer {
     _target: HTMLDivElement;
     _handlePlayPauseButtonClicked: Function;
     _handleVolumeClicked: Function;
+    _playVideoCallback: Function;
+    _hlsManager: HlsManager;
 
     constructor(
         representation: Representation,
@@ -29,8 +30,14 @@ export default class SimpleAVRenderer extends BaseRenderer {
         analytics: AnalyticsLogger,
     ) {
         super(representation, assetCollectionFetcher, fetchMedia, player, analytics);
-        if (Hls.isSupported()) {
-            this._hls = new Hls({ startFragPrefetch: true, startLevel: 3 });
+        this._handlePlayPauseButtonClicked = this._handlePlayPauseButtonClicked.bind(this);
+        this._handleVolumeClicked = this._handleVolumeClicked.bind(this);
+        this._playVideoCallback = this._playVideoCallback.bind(this);
+
+        this._hlsManager = player._hlsManager;
+
+        if (this._hlsManager.isSupported()) {
+            this._hls = this._hlsManager.getHlsFromPool();
         }
         this.renderVideoElement();
         this._handlePlayPauseButtonClicked = this._handlePlayPauseButtonClicked.bind(this);
@@ -65,25 +72,44 @@ export default class SimpleAVRenderer extends BaseRenderer {
         this.playVideo();
     }
 
+    end() {
+        this._videoElement.pause();
+
+        try {
+            this._clearBehaviourElements();
+            this._target.removeChild(this._videoElement);
+        } catch (e) {
+            //
+        }
+
+        const player = this._player;
+        player.removeVolumeControl(this._representation.id);
+        player.disconnectScrubBar();
+        player.removeListener(
+            PlayerEvents.PLAY_PAUSE_BUTTON_CLICKED,
+            this._handlePlayPauseButtonClicked,
+        );
+        player.removeListener(
+            PlayerEvents.VOLUME_CHANGED,
+            this._handleVolumeClicked,
+        );
+    }
+
+    _playVideoCallback(): void {
+        if (this._destroyed) {
+            logger.warn('loaded destroyed video element - not playing');
+        } else {
+            this._videoElement.play();
+        }
+    }
+
     playVideo() {
         if (this._videoElement.readyState >= this._videoElement.HAVE_CURRENT_DATA) {
             this._videoElement.play();
         } else if (this._videoElement.src.indexOf('m3u8') !== -1) {
-            this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                if (this._destroyed) {
-                    logger.warn('loaded destroyed video element - not playing');
-                } else {
-                    this._videoElement.play();
-                }
-            });
+            this._hls.on(HlsManager.Events.MANIFEST_PARSED, this._playVideoCallback);
         } else {
-            this._videoElement.addEventListener('loadeddata', () => {
-                if (this._destroyed) {
-                    logger.warn('loaded destroyed video element - not playing');
-                } else {
-                    this._videoElement.play();
-                }
-            });
+            this._videoElement.addEventListener('loadeddata', this._playVideoCallback);
         }
     }
 
@@ -191,7 +217,7 @@ export default class SimpleAVRenderer extends BaseRenderer {
         if (this._videoElement.readyState >= this._videoElement.HAVE_CURRENT_DATA) {
             this._videoElement.currentTime = time;
         } else if (this._videoElement.src.indexOf('m3u8') !== -1) {
-            this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            this._hls.on(HlsManager.Events.MANIFEST_PARSED, () => {
                 this._videoElement.currentTime = time;
             });
         } else {
@@ -202,7 +228,7 @@ export default class SimpleAVRenderer extends BaseRenderer {
     }
 
     switchFrom() {
-        this.destroy();
+        this.end();
     }
 
     switchTo() {
@@ -217,23 +243,11 @@ export default class SimpleAVRenderer extends BaseRenderer {
     }
 
     destroy() {
-        try {
-            this._clearBehaviourElements();
-            this._target.removeChild(this._videoElement);
-        } catch (e) {
-            //
-        }
+        this.end();
 
-        const player = this._player;
-        player.removeVolumeControl(this._representation.id);
-        player.removeListener(
-            PlayerEvents.PLAY_PAUSE_BUTTON_CLICKED,
-            this._handlePlayPauseButtonClicked,
-        );
-        player.removeListener(
-            PlayerEvents.VOLUME_CHANGED,
-            this._handleVolumeClicked,
-        );
+        this._hlsManager.returnHlsToPool(this._hls);
+
+        delete this._videoElement;
 
         super.destroy();
     }
