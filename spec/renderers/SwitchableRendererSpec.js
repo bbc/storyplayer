ff3949969c52de8086c3796e0a1feb9166d6db78
Thomas Preece <thomas.preece@bbc.co.uk>
2018-03-13 15:37:58 +0000

    Fixed flow errors introducted in last commit

--- spec/renderers/SwitchableRendererSpec.js
+++ spec/renderers/SwitchableRendererSpec.js
@@ -1,6 +1,7 @@
 // @flow
 
 import 'babel-polyfill';
+// @flowignore
 import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
 import SwitchableRenderer from '../../src/renderers/SwitchableRenderer';
