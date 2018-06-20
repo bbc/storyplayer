4ec1f61a6b1ff9020ee83d9a080fb224770d290f
Thomas Preece <thomas.preece@bbc.co.uk>
2018-06-20 11:58:09 +0100

    Added an upload story endpoint to the romper demo

--- demos.js
+++ demos.js
@@ -1,15 +1,34 @@
-var express = require('express'),
-  path = require('path'),
-  app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
 
-//set the port
+const app = express();
+
+// set the port
 app.set('port', 3000);
 
-//tell express that we want to use the www folder
-//for our static assets
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Upload JSON story and playback
+// WARNING: This is example code and should NEVER be deployed to a server as the following
+// upload function could easily be used for mallious purposes
+app.post('/upload', (request, respond) => {
+    const id = uuid();
+    const filePath = `${__dirname}/tmp/${id}.json`;
+    fs.appendFile(filePath, request.body.storyjson, () => {
+        respond.redirect(`/examples/?storyjson=../tmp/${id}.json`);
+    });
+});
+
+// tell express that we want to use the www folder
+// for our static assets
 app.use(express.static(path.join(__dirname, './')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function () {
-  console.log('The server is running: view the demos on http://localhost:3000/examples/');
+app.listen(app.get('port'), () => {
+    console.log('The server is running: view the demos on http://localhost:3000/examples/');
 });
