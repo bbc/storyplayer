1b4d23851c6025665017edd7b54244a29982f429
Andy Brown <andy.brown01@bbc.co.uk>
2018-11-13 14:25:46 +0000

    Bug fix fun (#130)
    * Hack to ensure setCurrentTime actually happens.
    
    * Fix restart failures
    
    * set start image z-index to ensure visible over videos on restart
    
    * enable switchable syncing with in-points
    
    * slightly smoother switch sync
    
    * Fix switch not restarting bug
    
    * Ensure media pause on reaching out time
    
    * tweak
    
    * indecision
    
    * Fixed z-index things

--- src/RenderManager.js
+++ src/RenderManager.js
@@ -152,8 +152,12 @@ export default class RenderManager extends EventEmitter {
                 .then(representationCollection =>
                     this._representationReasoner(representationCollection))
                 .then((representation) => {
-                    if (this._currentNarrativeElement === narrativeElement) {
-                        // Restarting Current NE
+                    if (this._currentNarrativeElement
+                            && this._currentNarrativeElement.id === narrativeElement.id
+                            && this._currentRenderer
+                            // need to use _representation as switchable getRepresentation
+                            // reports current choice id
+                            && this._currentRenderer._representation.id === representation.id) {
                         this._restartCurrentRenderer();
                     } else {
                         // get a Renderer for this new NE
@@ -372,6 +376,8 @@ export default class RenderManager extends EventEmitter {
             currentRenderer.end();
             currentRenderer.willStart();
             this._showOnwardIcons();
+        } else {
+            logger.error('no current renderer to restart');
         }
     }
 
