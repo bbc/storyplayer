7b9a7e69cd47be0f23683379de1010eeedc9a181
Andy Brown <andy.brown01@bbc.co.uk>
2019-08-06 12:48:19 +0100

    Threejs image timings (#297)
    * image360 representations honour duration
    * Hook up video to scrub bar
    * disable scrub bar and try to fix CORS
    * Better handling of time events, to match ImageRenderer
    * version bump

--- src/renderers/ThreeJsImageRenderer.js
+++ src/renderers/ThreeJsImageRenderer.js
@@ -9,10 +9,28 @@ import logger from '../logger';
 
 const THREE = require('three');
 
+const TIMER_INTERVAL = 100;
+
 export default class ThreeJsVideoRenderer extends ThreeJsBaseRenderer {
     _fetchMedia: MediaFetcher;
 
     _imageElement: HTMLImageElement;
+
+    _imageTimer: ?IntervalID;
+
+    _timeElapsed: number;
+
+    _duration: number;
+
+    _timedEvents: { [key: string]: Object };
+
+    _disablePlayButton: Function;
+
+    _disableScrubBar: Function;
+
+    _enablePlayButton: Function;
+
+    _enableScrubBar: Function;
 
     constructor(
         representation: Representation,
@@ -31,19 +49,100 @@ export default class ThreeJsVideoRenderer extends ThreeJsBaseRenderer {
             controller,
         );
 
+        this._disablePlayButton = () => { this._player.disablePlayButton(); };
+        this._enablePlayButton = () => { this._player.enablePlayButton(); };
+        this._disableScrubBar = () => { this._player.disableScrubBar(); };
+        this._enableScrubBar = () => { this._player.enableScrubBar(); };
         this.renderImageElement();
+        this._timeElapsed = 0;
+        this._duration = Infinity;
+        this._timedEvents = {};
     }
 
     start() {
         super.start();
         logger.info('Starting ThreeJs image');
-        if(this._rendered) {
+        if (this._rendered) {
             this._showImage();
+        }
+        if (this._representation.duration && this._representation.duration > 0){
+            this._duration = this._representation.duration;
+            this._timeElapsed = 0;
+            // eslint-disable-next-line max-len
+            logger.info(`360 Image representation ${this._representation.id} timed for ${this._representation.duration}s, starting now`);
+        }
+        if (this._representation.duration && this._representation.duration === 0) {
+            this.complete();
+        } else {
+            this._startTimer();
+        }
+        this._disableScrubBar();
+        this._disablePlayButton();
+    }
+
+    pause() {
+        clearInterval(this._imageTimer);
+    }
+
+    _startTimer() {
+        this._imageTimer = setInterval(() => {
+            this._timeElapsed += TIMER_INTERVAL/1000;
+            if (this._timeElapsed >= this._duration) {
+                // eslint-disable-next-line max-len
+                logger.info(`Image representation ${this._representation.id} completed timeout`);
+                this.complete();
+            }
+            Object.keys(this._timedEvents).forEach((timeEventId) => {
+                const { time, callback } = this._timedEvents[timeEventId];
+                if (this._timeElapsed >= time){
+                    delete this._timedEvents[timeEventId];
+                    callback();
+                }
+            });
+        }, TIMER_INTERVAL);
+    }
+
+    getCurrentTime(): Object {
+        if (this._representation.duration && this._representation.duration > 0) {
+            const timeObject = {
+                timeBased: true,
+                currentTime: this._timeElapsed,
+                remainingTime: this._duration - this._timeElapsed,
+            };
+            return timeObject;
+        }
+        return super.getCurrentTime();
+    }
+
+    play(){
+        this._startTimer();
+    }
+
+    addTimeEventListener(listenerId: string, time: number, callback: Function) {
+        this._timedEvents[listenerId] = { time, callback };
+    }
+
+    deleteTimeEventListener(listenerId: string) {
+        if (listenerId in this._timedEvents) {
+            delete this._timedEvents[listenerId];
         }
     }
 
+    end() {
+        super.end();
+        if (this._imageTimer){
+            clearInterval(this._imageTimer);
+        }
+        this._timedEvents = {};
+        this._enablePlayButton();
+        this._enableScrubBar();
+    }
+
     _showImage() {
-        const texture = new THREE.TextureLoader().load(this._imageElement.src);
+        const loader = new THREE.TextureLoader();
+        loader.setCrossOrigin('');
+        loader.load(this._imageElement.src);
+        const texture = loader.load(this._imageElement.src);
         const material = new THREE.MeshBasicMaterial({ map: texture });
 
         const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
@@ -57,7 +156,7 @@ export default class ThreeJsVideoRenderer extends ThreeJsBaseRenderer {
     }
 
     renderImageElement() {
-        // set video source
+        // set image source
         if (this._representation.asset_collections.foreground_id) {
             this._fetchAssetCollection(this._representation.asset_collections.foreground_id)
                 .then((fg) => {
@@ -67,7 +166,7 @@ export default class ThreeJsVideoRenderer extends ThreeJsBaseRenderer {
                                 this.populateImageElement(mediaUrl)
                             )
                             .catch((err) => {
-                                logger.error(err, 'Video not found');
+                                logger.error(err, 'Image not found');
                             });
                     }
                 });
@@ -77,6 +176,7 @@ export default class ThreeJsVideoRenderer extends ThreeJsBaseRenderer {
     populateImageElement(mediaUrl: string) {
         this._imageElement = document.createElement('img');
         this._imageElement.src = mediaUrl;
+        this._imageElement.crossOrigin = 'Anonymous';
         this._rendered = true;
         if(this._started) {
             this._showImage();
