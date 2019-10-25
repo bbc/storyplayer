fc18f9edbf7a3e4dcebb3df693662e623ca52703
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2019-10-25 19:28:11 +0100

    fix: ignore scrub bar activity  if duration infinite

--- src/Player.js
+++ src/Player.js
@@ -1836,6 +1836,8 @@ class Player extends EventEmitter {
         const scrubBarChangeFunc = () => {
             // Calculate the new time
             const { duration } = renderer.getCurrentTime();
+            if (duration === Infinity) return;
+            
             const time = duration * (parseInt(scrubBar.value, 10) / 100);
             renderer.setCurrentTime(time);
 
@@ -1862,8 +1864,10 @@ class Player extends EventEmitter {
             const percent = e.offsetX / scrubBar.offsetWidth;
             const { duration } = renderer.getCurrentTime();
             // Update the media time
-            const newTime = percent * duration;
-            renderer.setCurrentTime(newTime);
+            if (duration !== Infinity) {
+                const newTime = percent * duration;
+                renderer.setCurrentTime(newTime);
+            }
         });
 
         let isDragging = false;
