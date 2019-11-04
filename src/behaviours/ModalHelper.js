50ffaa25e561d452a85b49673235fca0e2a1d3b6
Andy Brown <andy.brown01@bbc.co.uk>
2019-11-04 14:22:56 +0000

    fix: modal position respects zeros (#364)
    * fix: respect zero values for modal positions
    
    * chore: version bump

--- src/behaviours/ModalHelper.js
+++ src/behaviours/ModalHelper.js
@@ -3,12 +3,12 @@
 /* eslint-disable no-param-reassign */
 const setDefinedPosition = (modalElement, behaviour) => {
     const { top, left, width, height, bottom, right } = behaviour.position;
-    if (top) modalElement.style.top = `${top}%`;
-    if (left) modalElement.style.left = `${left}%`;
-    if (width) modalElement.style.width = `${width}%`;
-    if (height) modalElement.style.height = `${height}%`;
-    if (bottom) modalElement.style.bottom = `${bottom}%`;
-    if (right) modalElement.style.right = `${right}%`;
+    if (top !== undefined && top !== null) modalElement.style.top = `${top}%`;
+    if (left !== undefined && left !== null) modalElement.style.left = `${left}%`;
+    if (width !== undefined && width !== null) modalElement.style.width = `${width}%`;
+    if (height !== undefined && height !== null) modalElement.style.height = `${height}%`;
+    if (bottom !== undefined && bottom !== null) modalElement.style.bottom = `${bottom}%`;
+    if (right !== undefined && right !== null) modalElement.style.right = `${right}%`;
 };
 /* eslint-enable no-param-reassign */
 
