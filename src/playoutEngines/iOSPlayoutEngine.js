5d3cdae17bc96d20d0e172cb595a7f493ba3f515
Andy Jones <andy.jones01@bbc.co.uk>
2019-11-06 17:02:06 +0000

    remove unused import

--- src/playoutEngines/iOSPlayoutEngine.js
+++ src/playoutEngines/iOSPlayoutEngine.js
@@ -4,7 +4,6 @@ import BasePlayoutEngine, { MEDIA_TYPES } from './BasePlayoutEngine';
 import MediaManager from './srcSwitchPlayoutEngine/MediaManager';
 import Player, { PlayerEvents } from '../Player';
 import logger from '../logger';
-import { BrowserUserAgent } from '../browserCapabilities';
 
 export default class iOSPlayoutEngine extends BasePlayoutEngine {
     _foregroundMediaElement: HTMLVideoElement
