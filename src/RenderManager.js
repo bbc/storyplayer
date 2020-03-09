589bc723fd59771c80f0e61144d9954b507958f1
Andy Brown <andy.brown01@bbc.co.uk>
2020-03-09 14:15:32 +0000

    fix: do not play renderer on visibility change if already finished (#417)
    Co-authored-by: Thomas Preece <thomaspreeceBBC@users.noreply.github.com>

--- src/RenderManager.js
+++ src/RenderManager.js
@@ -204,9 +204,9 @@ export default class RenderManager extends EventEmitter {
         } else {
             // pause the timer for the current representation
             if (this._isPlaying) {
-                if (this._currentRenderer) this._currentRenderer.play();
                 // unless it has already ended, set it going again
                 if (this._currentRenderer && !this._currentRenderer.hasEnded()) {
+                    if (this._currentRenderer) this._currentRenderer.play();
                     this._player.playoutEngine.play();
                 }
             }
