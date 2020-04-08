f7bce04abc23b4cf403f66d3aa31d4ce44647fb1
Andy Jones <andy.jones01@bbc.co.uk>
2020-04-08 17:45:58 +0100

    started electron  app

--- storyplayer-electron/src/index.js
+++ storyplayer-electron/src/index.js
@@ -1,10 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
-}
 
 const createWindow = () => {
     // Create the browser window.
