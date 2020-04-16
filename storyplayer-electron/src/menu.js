0c29daf8c48b559e86a6c06fc6eccb13454aad03
Andy Jones <andy.jones01@bbc.co.uk>
2020-04-16 11:48:34 +0100

    v0.3.3

---
+++ storyplayer-electron/src/menu.js
@@ -0,0 +1,56 @@
+const { Menu } = require('electron');
+const isDevMode = require('electron-is-dev');
+
+const createAppMenu = () => {
+    const appMenuTemplate = [
+        {
+            label: "Edit",
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'delete' },
+                { role: 'selectall' }
+            ]
+        },
+        {
+            label: "View",
+            submenu: [
+                { role: 'reload' },
+                { type: 'separator' },
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }                
+            ]
+        },
+        {
+            role: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'close' }
+            ]
+        }, 
+    ];
+    if (isDevMode) {
+        appMenuTemplate.push({
+            label: "Debug",
+            submenu: [
+                { role: 'forcereload' },
+                { role: 'toggledevtools' },
+            ],
+        });
+    }
+    Menu.setApplicationMenu(
+        Menu.buildFromTemplate(appMenuTemplate)
+    );
+
+};
+
+module.exports = {
+    createAppMenu,
+};
