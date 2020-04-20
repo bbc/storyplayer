f804ba0249f7810d7d791d198e4b8bc3dc83c7b0
Andy Jones <andy.jones01@bbc.co.uk>
2020-04-20 12:23:44 +0100

    updated language

--- storyplayer-electron/src/renderers/rendererPreload.js
+++ storyplayer-electron/src/renderers/rendererPreload.js
@@ -8,6 +8,19 @@ const pageDidLoad = () => {
     if(homeButton) {
         homeButton.addEventListener("click", showHomePage); 
     }
+
+    const documentsElement = document.getElementById('documents-folder');
+
+    if(documentsElement) {
+        if(process.platform === 'darwin') {
+            documentsElement.textContent = 'Home Documents folder, usually this is in your home or user folder.';
+        } else if(process.platform === 'win32') {
+            documentsElement.textContent = 'My Documents folder, usually this is in your user folder.';
+        }
+        else {
+            documentsElement.textContent = 'Home Documents folder, usually this is in your user profile folder.';
+        }
+    }
 };
 
 
