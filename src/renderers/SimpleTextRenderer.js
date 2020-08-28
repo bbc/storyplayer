df08dcf8bd977b9071d95874a6f746d1152848f8
Andy Jones <andy.jones01@bbc.co.uk>
2020-08-28 13:13:49 +0100

    overflowing text now scrolls

--- src/renderers/SimpleTextRenderer.js
+++ src/renderers/SimpleTextRenderer.js
@@ -7,6 +7,7 @@ import type { AnalyticsLogger } from '../AnalyticEvents';
 import Controller from '../Controller';
 
 import logger from '../logger';
+import { REASONER_EVENTS } from '../Events';
 
 export type HTMLTrackElement = HTMLElement & {
     kind: string,
@@ -28,6 +29,9 @@ export default class SimpleTextRenderer extends BaseRenderer {
 
     _textDiv: HTMLDivElement;
 
+    _setGUILayerCSS: Function;
+
+    MAX_HEIGHT: number;
 
     constructor(
         representation: Representation,
@@ -45,8 +49,12 @@ export default class SimpleTextRenderer extends BaseRenderer {
             analytics,
             controller,
         );
-
         this._target = player.mediaTarget;
+        this._setGUILayerCSS = this._setGUILayerCSS.bind(this);
+
+        // we have a one time event listener as we remove the prestart classname from the media element to indicate we have started
+        // otherwise the GUI is shrunk and buttons disappear
+        this._player.once(REASONER_EVENTS.ROMPER_STORY_STARTED, this._setGUILayerCSS);
     }
 
     async init() {
@@ -59,6 +67,9 @@ export default class SimpleTextRenderer extends BaseRenderer {
         }
     }
 
+    /**
+     * Called to actually start the renderer
+     */
     willStart() {
         const ready = super.willStart();
         if (!ready) return false;
@@ -66,15 +77,24 @@ export default class SimpleTextRenderer extends BaseRenderer {
         this._target.appendChild(this._textDiv);
         this._player.disablePlayButton();
         this._player.disableScrubBar();
+        this._setGUILayerCSS();
         return true;
     }
 
+    /**
+     * Starts the renderer sets the phase to be RENDERER_PHASES.MEDIA_FINISHED
+     * @extends BaseRenderer#start()
+     */
     start() {
         super.start();
         // no duration, so ends immediately
         this._setPhase(RENDERER_PHASES.MEDIA_FINISHED);
     }
 
+    /**
+     * End the renderer, and clean up the css classes on the GUI later if we need to
+     * @extends baseRenderer#end()
+     */
     end() {
         const needToEnd = super.end();
         if (!needToEnd) return false;
@@ -159,6 +179,49 @@ export default class SimpleTextRenderer extends BaseRenderer {
         return Promise.resolve(textContent);
     }
 
+    isOverflown() {
+        return this._textDiv.scrollHeight > this._textDiv.clientHeight;
+    }
+
+    isNotPreStart() {
+        return !this._target.classList.contains('romper-prestart');
+    }
+
+    /**
+     * Sets the overflow style for the text, on the gui layer and the text div too
+     * @param {HTMLElement} guiLayer 
+     */
+    setOverflowStyle(guiLayer: HTMLElement) {
+        if (this.isOverflown()) {
+            if (this.isNotPreStart()) {
+                guiLayer.classList.add('overflowing-text');
+                this._textDiv.style.overflow = 'scroll';
+                this._textDiv.style['max-height'] = `${this.MAX_HEIGHT}px`;
+            }
+        } else {
+            guiLayer.classList.remove('overflowing-text');
+            this._textDiv.style.overflow = '';
+            this._textDiv.style['max-height'] = '';
+        }       
+    }
+    
+    /**
+     * Gets the gui layer and checks we have added the text node to the parent, 
+     * then sets the CSS style appropriately
+     */
+    // eslint-disable-next-line class-methods-use-this
+    _setGUILayerCSS() {
+        const guiLayer = document.getElementById('gui-layer');
+        if(guiLayer && this._textDiv.parentNode) {
+            this.setOverflowStyle(guiLayer);
+        }
+    }
+
+    /**
+     * Populates the text element with the string provided
+     * And sets the GUI to be the bottom 20% of the screen
+     * @param {string} textContent 
+     */
     populateTextElement(textContent: string) {
         this._replaceEscapedVariables(textContent)
             .then((newText) => {
