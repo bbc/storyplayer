f7b7a726b6d3130660f4be2dd5b55bb40d19d990
Andy Brown <andy.brown01@bbc.co.uk>
2022-12-22 12:47:17 +0000

    chore: remove all jest tests

--- spec/decls/mocha.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-declare function describe(description: string, callback: Function): void;
-declare function beforeEach(callback: Function): void;
-declare function it(description: string, callback: Function): void;
