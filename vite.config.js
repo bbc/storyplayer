0636a506027cfba8f335ba1828aac8d2b154e301
dannymeloy <danny.meloy@bbc.co.uk>
2023-08-16 15:57:59 +0100

    CI: Add to vite config, compile as ES Module and note commonJS

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  publicDir: 'src/assets/public',
-  build: {
-    target: 'es2015',
-    lib: {
-      entry: resolve(__dirname, 'src/storyplayer.ts'),
-      name: 'StoryPlayer',
-      fileName: (format, entryName) => `${entryName}.js`,
-      formats: ['cjs'],
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: "storyplayer.[ext]",
+export default defineConfig(({ command, mode, ssrBuild }) => {
+
+  const isProduction = mode === "production";
+  const minify = isProduction ? "terser" : "esbuild"; // applies to src code and css
+  const cssCodeSplit = isProduction;
+  const sourcemap = isProduction ? false : "inline";
+
+  return {
+    build: {
+      target: 'es2015',
+      cssCodeSplit,
+      minify,
+      sourcemap,
+      lib: {
+        entry: resolve(__dirname, 'src/storyplayer.ts'),
+        name: 'StoryPlayer',
+        fileName: (format, entryName) => `${entryName}.js`,
+        formats: ['es'],
+      },
+      rollupOptions: {
+        output: {
+          assetFileNames: "storyplayer.[ext]",
+        },
       },
     },
-  },
+  }
 })
